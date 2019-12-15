6dcba94a36bd9e0c7050ae44db94d16501d73c7f
Joshua Lo <josh.lol1995@gmail.com>
2019-12-14 17:36:02 -0800

    Disregard previous commit. Javascript has been freed of most bugs. Elements have been positioned and styled using bootstrap and css.

--- script.js
+++ script.js
@@ -18,23 +18,26 @@
 
 // Your application's GitHub repository should contain a README.md file explaining the purpose and functionality of the application. The README.md file should include a screenshot of the completed application as well as a link to the deployed GitHub Pages URL.
 
+lowerAlphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+upperAlphabet = [];
+
+//Created a for loop to fill the upper alphabet because lazy
+for (let i = 0; i < lowerAlphabet.length; i++) {
+    upperAlphabet.push(lowerAlphabet[i].toUpperCase());
+}
+
+
 // Initializing array to store allowed charactrers for the password
-
+// Initializing all boolean values with prompts
 function init() {
-    lowerAlphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    upperAlphabet = [];
-
-    //Created a for loop to fill the upper alphabet because lazy
-    for (let i = 0; i < lowerAlphabet.length; i++) {
-        upperAlphabet.push(lowerAlphabet[i].toUpperCase());
-    }
-
     let charsAllowed = [];
 
     // Get user input 
+
+    // Bug where "undefined" is being printed when using special characters only
     let specialsPrompt = confirm("Do you want to have special characters in your password?");
     if(specialsPrompt) {
-        charsAllowed.push('!','@','#','$','%','^','&','*','(',')','-','+','=','`','~',"/","\\",'{','}','[',']');
+        charsAllowed.push('!','@','#','$','%','^','&','*','(',')','-','+','=','\`', '\"','~',"\/","\\",'{','}','[',']');
     }
 
     let numbersPrompt = confirm("Do you want numbers in your password?");
@@ -56,32 +59,65 @@ function init() {
             charsAllowed.push(upperAlphabet[i]);
         }
     }
-    let pwl = prompt("How many characters long would you like your password to be? (type an integer between 8 and 128)");
 
-    while (pwl < 8 || pwl > 128) {
-        pwl = prompt("Try again, pick a number between 8 and 128");
+    if (!specialsPrompt && !numbersPrompt && !lowersPrompt && !uppersPrompt) {
+        alert("Need to have characters to put into password.");
+        init();
+
     }
 
+
+    // While loop that ensures user input is valid by checking type. Also gives user an option to exit out of the program.
+    let pwl = null;
+    while(true) {
+        pwl = prompt("How many characters long would you like your password to be? Type an integer between and including 8 and 128.");
+
+        if (pwl == null) {
+            alert("You have exited the program.");
+            break;
+        } else if (pwl.length <= 0 || isNaN(pwl)) {
+            pwl = alert("Invalid input. Please input an integer between and include 8 and 128.");        
+        } else if (parseInt(pwl) < 8 || parseInt(pwl) > 128) {
+            alert("Integer not in range. Please input an integer between and including 8 and 128.");
+        } else {
+            pwl = parseInt(pwl);
+            break;
+        }
+    }
+
+    // End user input
+    
     let pw = "";
-    // Creating password
+    // Creating password through concatanation.
     for(let i = 0; i < pwl; i++) {
-        let selector = Math.round(Math.random() * charsAllowed.length);
+        let selector = Math.round(Math.random() * (charsAllowed.length - 1));
         pw += charsAllowed[selector];
+
+        // DO NOT DELETE, FOR TESTING WHEN UNDEFINED VALUES ARE RETURNED
+        // console.log(selector);
+        // console.log(charsAllowed.length);
+        // console.log(pw);
     }
 
+    // Sets the password field to the generated password
     document.getElementById("password").textContent = pw;
-
 }
 
+// End Init
 
 
-    // Generating a new password
+
+
+
+    // Generate a new password
     document.getElementById('generate').addEventListener("click", function() {
-        console.log('Generate button pressed!')
         init();
     });
+    // End generate a new password
 
-    // Copying the generated passsword
+
+
+    // Copy the generated passsword
     document.getElementById('copy').addEventListener("click", function() {
         var test = document.getElementById("password");
 
@@ -92,5 +128,6 @@ function init() {
 
         alert("Copied the text: " + test.value)
     });
+    // End copy the generated password
 
 
